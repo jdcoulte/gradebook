29356dd3b7cf77f4e5d05d7bb18414a3c10fa349
Jeff C - Desktop <jeff.coulter1@gmail.com>
2020-01-17 14:58:48 +0100

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var book = new Book();
+            book.AddGrade(89.1);
+
+            var grades = new List<double>() {33.6, 22.1, 18.4, 39.1};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}.");
+            
+ 
+        }
+    }
+}
