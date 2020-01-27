512bf8e30ed0cd1e2e280ef19226311d3f33f28d
Jeff C - Desktop <jeff.coulter1@gmail.com>
2020-01-27 10:49:34 +0100

    Updates 1/26/2020

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,18 +10,38 @@ namespace GradeBook
         {   
             
             var book = new Book("class1");
-            book.AddGrade(89.1);
-            book.AddGrade(65.8);
-            book.AddGrade(80.4);
-            book.AddGrade(100.0);
-            book.GetStatistics();
-            
+
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+                
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The average is {stats.Average:N1}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The letter grade is {stats.Letter}")
+            Console.WriteLine($"The letter grade is {stats.Letter}");
  
         }
     }
